f0d4087ca74bcaf74f46b889dff7d50a81f32eec
Judson <nyarly@gmail.com>
2012-10-24 02:38:03 -0700

    Couple of bugs

--- lib/logical-construct/ground-control/setup/install-init.rb
+++ lib/logical-construct/ground-control/setup/install-init.rb
@@ -4,16 +4,18 @@ require 'logical-construct/ground-control/run-on-target'
 module LogicalConstruct
   module GroundControl
     class InstallInit < RunOnTarget
-      required_settings :source_path, :target_path
+      required_fields :source_path, :target_path
       setting :construct_dir
       setting :service_name, "logical-construct"
       setting :initd_path, "/etc/init.d"
 
       def default_configuration(setup)
+        super
         setup.copy_settings_to(self)
       end
 
       def resolve_configuration
+        super
         self.source_path ||= File::join(construct_dir, "construct.init.d")
         self.target_path ||= File::join(initd_path, service_name)
       end
